bad1cf510f9518e3d6286e1dfa0159d9b3fc6d13
Di-kairos <205350801+Di-kairos@users.noreply.github.com>
2026-06-25 09:58:55 +0300

    chore(formula): bump to v0.1.3 (sha256 of source tarball)

--- Formula/panic.rb
+++ Formula/panic.rb
@@ -1,8 +1,8 @@
 class Panic < Formula
   desc "One-step hide-and-lock kill-switch for macOS"
   homepage "https://github.com/Di-kairos/panic"
-  url "https://github.com/Di-kairos/panic/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "e8ea6ace3768543a5e88be02162f4ed7ac2f48d3c555d392e106d8b0b5c3a1ed"
+  url "https://github.com/Di-kairos/panic/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "7e5e9891cb3c0e61cb0a263c9dc0c0dcf0f78d0f1d514ab8ab16dd2e8fb2e7cb"
   license "MIT"
 
   def install
